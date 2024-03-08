dec21e66fabeece5c42dd93941932144846f580d
Valentinas <114878006+Valentinas-P@users.noreply.github.com>
2024-03-08 17:24:38 +0000

    Update main.py with error handling
    An updated version of using error handling to catch error's.

--- NATO Alphabet Converter/main.py
+++ NATO Alphabet Converter/main.py
@@ -1,13 +1,16 @@
 import pandas
 
-# TODO 1. Create a dictionary in this format:
 alphabetic_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabetic_dict = {row.letter: row.code for index, row in alphabetic_data.iterrows()}
-print(alphabetic_dict)
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Type your name: ").strip("").upper()
-letters_to_start = list(user_input)
+def nato_alphabet():
+    try:
+        user_input = input("Type your name: ").strip("").upper()
+        new_list = [alphabetic_dict[letter] for letter in user_input]
+        print(new_list)
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        nato_alphabet()
 
-new_list = [alphabetic_dict[letter] for letter in user_input]
-print(new_list)
+
+nato_alphabet()
